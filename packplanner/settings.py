bf794de2a625b0845785a9e4ce48ee8bcb382c82
Andrew Moran <andrewmo@mit.edu>
2013-05-02 19:08:27 -0400

    Revert "Got user accounts working, added view individual model pages, added page title, tried playing around with js anf django template tags in schedules page but stopped"
    This reverts commit f068590b201ce4d4d9c43b6aa835d26b889fcddf.

--- packplanner/settings.py
+++ packplanner/settings.py
@@ -12,13 +12,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'd4mn8e848q7ojr',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'qrkupvnfpvsgom',
-        'PASSWORD': 'XPPBp5zmodD-6dToMXzCHjOWVb',
-        'HOST': 'ec2-23-21-89-65.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
--- packplanner/views.py
+++ packplanner/views.py
@@ -1,42 +1,19 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
-@login_required
 def index(request):
-	return render(request, 'index.html', {})
+    return render(request, 'index.html', {})
 
-@login_required
 def calendar(request):
-	return render(request, 'index.html', {})
+    return render(request, 'index.html', {})
 
-@login_required
 def contacts(request):
-	return render(request, 'contacts.html', {})
+    return render(request, 'contacts.html', {})
 
-@login_required
 def inbox(request):
-	return render(request, 'inbox.html', {})
+    return render(request, 'inbox.html', {})
 
-@login_required
 def schedules(request):
-	return render(request, 'schedules.html', {})
+    return render(request, 'schedules.html', {})
 
-@login_required
 def settings(request):
-	return render(request, 'settings.html', {})
-
-@login_required
-def view_contact(request, id):
-	return render(request, 'view-contact.html', {})
-
-@login_required
-def view_message(request, id):
-	return render(request, 'view-message.html', {})
-
-@login_required
-def view_schedule(request, id):
-	schedules = []
-	schedules.append({'id' : 1, 'name': "Soccer"})
-	schedules.append({'id' : 2, 'name': "Drama Rehearsals"})
-	schedules.append({'id' : 3, 'name': "Girl Scouts"})
-	return render(request, 'view-schedule.html', {'schedules' : schedules})
+    return render(request, 'settings.html', {})
