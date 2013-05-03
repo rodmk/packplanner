f5cde4331f11361615b3626305ff7433c14e95ea
Ari <avogel@mit.edu>
2013-05-03 12:25:06 -0400

    merged everything successfully, new contacts and inbox page, as well as models and admin

---
+++ packplanner/templates/js/schedules.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	{% for schedule in schedules %}
+	//var newScheduleLink = document.createElement('div');
+	var tile = $('<a>', {
+	    // id: 'schedule-{{schedule.id}}',
+	    // href: '/schedule/{{schedule.id}}/',
+	    // text: '{{schedule.name}}',
+	    id: 'schedule-1',
+	    href: '/schedule/1/',
+	    text: 'schedule name'
+	});
+	$("#scheduleList").append(tile);
+	{% endfor %}
+	//newScheduleLink.appendTo("#scheduleList");
+	//
+	// tile = $("<div>", {
+	// 	text: "A schedule in the list",
+	// });
+	// $("#scheduleList").append(tile);
+});
--- packplanner/urls.py
+++ packplanner/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,9 +12,14 @@ urlpatterns = patterns('',
     url(r'^home/$', 'packplanner.views.index'),
     url(r'^calendar/$', 'packplanner.views.calendar'),
     url(r'^contacts/$', 'packplanner.views.contacts'),
+    url(r'^contact/(\d+)/$', 'packplanner.views.view_contact'),
     url(r'^inbox/$', 'packplanner.views.inbox'),
+    url(r'^message/(\d+)/$', 'packplanner.views.view_message'),
     url(r'^schedules/$', 'packplanner.views.schedules'),
+    url(r'^schedule/(\d+)/$', 'packplanner.views.view_schedule'),
     url(r'^settings/$', 'packplanner.views.settings'),
+    (r'^accounts/login/$',  login),
+    (r'^accounts/logout/$', logout, {"next_page" : "/home/"}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
