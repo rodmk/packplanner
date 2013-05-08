017581b4922a652d9046900e87fcfb4591148e1a
rodmk <rodmk@mit.edu>
2013-05-08 03:31:40 -0400

    Got settings form working
    It's janky as all hell, but hey, it works!

--- packplanner/views.py
+++ packplanner/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, Http404
 from pack.models import *
+from django.shortcuts import *
 
 @login_required
 def index(request):
-	return HttpResponseRedirect('/calendar/')
+    return HttpResponseRedirect('/calendar/')
 
 @login_required
 def calendar(request):
-	family_member = get_family_member(request.user)
-	if family_member == None:
-		events_details = []
-	else:
-		events_details = FamilyEventDetails.objects.filter(family=family_member.family)
-	return render(request, 'index.html', {"events_details" : events_details})
+    family_member = get_family_member(request.user)
+    if family_member == None:
+        events_details = []
+    else:
+        events_details = FamilyEventDetails.objects.filter(family=family_member.family)
+    return render(request, 'index.html', {"events_details" : events_details})
 
 @login_required
 def contacts(request):
@@ -25,20 +26,24 @@ def contacts(request):
 
 @login_required
 def inbox(request):
-	return render(request, 'inbox.html', {})
+    return render(request, 'inbox.html', {})
 
 @login_required
 def schedules(request):
-	family_member = get_family_member(request.user)
-	if family_member == None:
-		schedules_details = []
-	else:
-		schedules_details = FamilyScheduleDetails.objects.filter(family=family_member.family)
-	return render(request, 'schedules.html', {"schedules_details" : schedules_details})
+    family_member = get_family_member(request.user)
+    if family_member == None:
+        schedules_details = []
+    else:
+        schedules_details = FamilyScheduleDetails.objects.filter(family=family_member.family)
+    return render(request, 'schedules.html', {"schedules_details" : schedules_details})
 
 @login_required
 def settings(request):
-	return render(request, 'settings.html', {})
+    family_member = get_family_member(request.user)
+    form = FamilyMemberForm(request.POST or None, instance=family_member)
+    if form.is_valid():
+        form.save()
+    return render(request, 'settings.html', {"user": get_family_member(request.user), "form": form})
 
 @login_required
 def view_contact(request, id):
@@ -46,16 +51,16 @@ def view_contact(request, id):
 
 @login_required
 def view_message(request, id):
-	family_member = get_family_member(request.user)
-	return render(request, 'view-message.html', {})
+    family_member = get_family_member(request.user)
+    return render(request, 'view-message.html', {})
 
 @login_required
 def view_schedule(request, id):
-	family_member = get_family_member(request.user)
+    family_member = get_family_member(request.user)
 
-	schedule = Schedule.objects.get(id=id)
-	return render(request, 'view-schedule.html', {"schedule" : schedule})
+    schedule = Schedule.objects.get(id=id)
+    return render(request, 'view-schedule.html', {"schedule" : schedule})
 
 def get_family_member(user):
-	family_member = user.user_account
-	return family_member
+    family_member = user.user_account
+    return family_member
